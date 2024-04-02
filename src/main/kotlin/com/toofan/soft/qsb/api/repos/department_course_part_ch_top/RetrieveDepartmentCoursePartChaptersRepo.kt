58008d3687c578dcc3f27880ad44c4a07a5caa48
M7D-99 <M7D.99.2022@gmail.com>
2024-04-02 03:40:13 +0300

    ......

--- src/main/kotlin/com/toofan/soft/qsb/api/repos/department_course_part_ch_top/RetrieveDepartmentCoursePartChaptersRepo.kt
+++ src/main/kotlin/com/toofan/soft/qsb/api/repos/department_course_part_ch_top/RetrieveDepartmentCoursePartChaptersRepo.kt
@@ -11,7 +11,8 @@ object RetrieveDepartmentCoursePartChaptersRepo {
         data: (
             mandatory: Mandatory
         ) -> Unit,
-        onComplete: (response: Response) -> Unit
+//        onComplete: (response: Response) -> Unit
+        onComplete: (Resource<List<Response.Data>>) -> Unit
     ) {
         var request: Request? = null
 
@@ -25,7 +26,13 @@ object RetrieveDepartmentCoursePartChaptersRepo {
                     route = Route.DepartmentCoursePartChapterAndTopic.RetrieveChapterList
                 ) {
                     val response = Response.map(it)
-                    onComplete(response)
+//                    onComplete(response)
+
+                    if (response.isSuccess) {
+                        onComplete(Resource.Success(response.data))
+                    } else {
+                        onComplete(Resource.Error(response.errorMessage))
+                    }
                 }
             }
         }
