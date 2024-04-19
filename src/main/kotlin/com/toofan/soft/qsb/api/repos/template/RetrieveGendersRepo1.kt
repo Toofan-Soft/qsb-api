9076a653a15727da186a66962dd06fbca0e1fe4e
M7D-99 <M7D.99.2022@gmail.com>
2024-04-20 02:27:06 +0300

    ......

---
+++ src/main/kotlin/com/toofan/soft/qsb/api/repos/template/RetrieveGendersRepo1.kt
@@ -0,0 +1,54 @@
+package com.toofan.soft.qsb.api.repos.template
+
+import com.google.gson.JsonObject
+import com.toofan.soft.qsb.api.*
+import com.toofan.soft.qsb.api.utils.InternetUtils
+
+object RetrieveGendersRepo1 {
+    @JvmStatic
+    suspend fun execute(
+        onComplete: (Resource<List<Response.Data>>) -> Unit
+    ) {
+        onComplete(
+            if (InternetUtils.isInternetAvailable()) {
+                Resource.Success(
+                    data = listOf(
+                        Response.Data(1, "Male"),
+                        Response.Data(2, "Female"),
+                    )
+                )
+            } else {
+                Resource.Error(
+                    message = "Internet is not available, check it then try again :)"
+                )
+            }
+        )
+    }
+
+    data class Response(
+        @Field("is_success")
+        val isSuccess: Boolean = false,
+        @Field("error_message")
+        val errorMessage: String? = null,
+        @Field("data")
+        val data: List<Data> = emptyList()
+    ) : IResponse {
+
+        data class Data(
+            @Field("id")
+            val id: Int = 0,
+            @Field("name")
+            val name: String = ""
+        )
+
+        companion object {
+            private fun getInstance(): Response {
+                return Response()
+            }
+
+            fun map(data: JsonObject): Response {
+                return getInstance().getResponse(data) as Response
+            }
+        }
+    }
+}
