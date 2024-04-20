98e53d34eeb90ed94cb8904525d098881e894347
M7D-99 <M7D.99.2022@gmail.com>
2024-04-21 01:13:52 +0300

    ...

---
+++ src/main/kotlin/com/toofan/soft/qsb/api/Coroutine.kt
@@ -0,0 +1,28 @@
+package com.toofan.soft.qsb.api
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+internal object Coroutine {
+    suspend fun launch(
+        block: suspend CoroutineScope.() -> Unit
+    ) {
+        val isDesktop = isDesktop()
+        println("isDesktop: $isDesktop")
+
+//        if (isDesktop()) CoroutineScope(Dispatchers.IO).launch(block = block)
+        if (isDesktop) CoroutineScope(Dispatchers.IO).launch(block = block)
+        else withContext(Dispatchers.IO, block)
+    }
+
+    private fun isDesktop(): Boolean {
+        return System.getProperty("os.name").lowercase().contains("windows") ||
+                System.getProperty("os.name").lowercase().contains("mac") ||
+                System.getProperty("os.name").lowercase().contains("linux")
+    }
+}
+
+fun main() {
+}
---
+++ src/main/kotlin/com/toofan/soft/qsb/api/repos/template/RetrieveGendersRepo4.kt
@@ -0,0 +1,56 @@
+package com.toofan.soft.qsb.api.repos.template
+
+import com.google.gson.JsonObject
+import com.toofan.soft.qsb.api.*
+import com.toofan.soft.qsb.api.utils.InternetUtils
+
+object RetrieveGendersRepo4 {
+    @JvmStatic
+    suspend fun execute(
+        onComplete: (Resource<List<Response.Data>>) -> Unit
+    ) {
+        Coroutine.launch {
+            onComplete(
+                if (InternetUtils.isInternetAvailable()) {
+                    Resource.Success(
+                        data = listOf(
+                            Response.Data(1, "Male"),
+                            Response.Data(2, "Female"),
+                        )
+                    )
+                } else {
+                    Resource.Error(
+                        message = "Internet is not available, check it then try again :)"
+                    )
+                }
+            )
+        }
+    }
+
+    data class Response(
+        @Field("is_success")
+        val isSuccess: Boolean = false,
+        @Field("error_message")
+        val errorMessage: String? = null,
+        @Field("data")
+        val data: List<Data> = emptyList()
+    ) : IResponse {
+
+        data class Data(
+            @Field("id")
+            val id: Int = 0,
+            @Field("name")
+            val name: String = ""
+        )
+
+        companion object {
+            private fun getInstance(): Response {
+                return Response()
+            }
+
+            fun map(data: JsonObject): Response {
+                return getInstance().getResponse(data) as Response
+            }
+        }
+    }
+}
