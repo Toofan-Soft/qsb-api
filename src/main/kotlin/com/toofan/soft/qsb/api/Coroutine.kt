2c330dde46586b5d757b47a4ca04002c6864dbcc
M7D-99 <M7D.99.2022@gmail.com>
2024-06-27 03:01:24 +0300

    ....

--- src/main/kotlin/com/toofan/soft/qsb/api/Coroutine.kt
+++ src/main/kotlin/com/toofan/soft/qsb/api/Coroutine.kt
@@ -1,9 +1,6 @@
 package com.toofan.soft.qsb.api
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 
 internal object Coroutine {
     suspend fun launch(
@@ -14,8 +11,8 @@ internal object Coroutine {
         println("isDesktop: $isDesktop")
         println("isAndroid: $isAndroid")
 
-//        if (isDesktop) CoroutineScope(Dispatchers.IO).launch(block = block)
-        if (isDesktop) withContext(Dispatchers.IO, block)
+        if (isDesktop) CoroutineScope(Dispatchers.IO).launch(block = block)
+//        if (isDesktop) withContext(Dispatchers.IO, block)
         else if (isAndroid) withContext(Dispatchers.IO, block)
         else withContext(Dispatchers.IO, block)
     }
