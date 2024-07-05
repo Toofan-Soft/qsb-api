5c1a5cb13f9e7b02140165def4a8dfd3c0f031db
M7D-99 <M7D.99.2022@gmail.com>
2024-07-06 01:59:03 +0300

    .........

--- src/main/kotlin/com/toofan/soft/qsb/api/repos/practice_exam/RetrievePracticeExamQuestionsRepo.kt
+++ src/main/kotlin/com/toofan/soft/qsb/api/repos/practice_exam/RetrievePracticeExamQuestionsRepo.kt
@@ -49,12 +49,20 @@ object RetrievePracticeExamQuestionsRepo {
         @Field("data")
         val data: List<Data> = emptyList()
     ) : IResponse {
-
         data class Data(
-            @Field("type_name")
-            val typeName: String = "",
-            @Field("questions")
-            val questions: List<Data> = emptyList()
+            @Field("id")
+            val id: Int = 0,
+            @Field("content")
+            val content: String = "",
+            @Field("attachment_url")
+            val attachmentUrl: String? = null,
+
+            @Field("user_answer")
+            private val userAnswer: Boolean? = null,
+            @Field("is_true")
+            private val isTrue: Boolean? = null,
+            @Field("choices")
+            private val choices: List<Data>? = null
         ) : IResponse {
             data class Data(
                 @Field("id")
@@ -63,106 +71,52 @@ object RetrievePracticeExamQuestionsRepo {
                 val content: String = "",
                 @Field("attachment_url")
                 val attachmentUrl: String? = null,
-
-                @Field("user_answer")
-                private val userAnswer: Boolean? = null,
+                @Field("is_selected")
+                val isSelected: Boolean? = null,
                 @Field("is_true")
-                private val isTrue: Boolean? = null,
-                @Field("choices")
-                private val choices: List<Data>? = null
-            ) : IResponse {
-                data class Data(
-                    @Field("id")
-                    val id: Int = 0,
-                    @Field("content")
-                    val content: String = "",
-                    @Field("attachment_url")
-                    val attachmentUrl: String? = null,
-                    @Field("is_selected")
-                    val isSelected: Boolean? = null,
-                    @Field("is_true")
-                    val isTrue: Boolean? = null
-                ) : IResponse
+                val isTrue: Boolean? = null
+            ) : IResponse
 
-                fun getChoices(): List<QuestionHelper.Data.Data> {
-                    return if (isTrue == null && userAnswer == null) {
-                        if (choices == null) {
-                            QuestionHelper.Data.Data.Type.values().map { it.toData() }
-                        } else {
-                            if (choices.all { it.isTrue == null && it.isSelected == null }) {
-                                choices.map {
-                                    QuestionHelper.Data.Data(
-                                        id = it.id,
-                                        content = it.content,
-                                        attachmentUrl = it.attachmentUrl
-                                    )
-                                }
-                            } else if (choices.all { it.isTrue != null && it.isSelected != null }) {
-                                choices.map {
-                                    QuestionHelper.Data.Data(
-                                        id = it.id,
-                                        content = it.content,
-                                        isTrue = it.isTrue,
-                                        isSelected = it.isSelected,
-                                        attachmentUrl = it.attachmentUrl
-                                    )
-                                }
-                            } else {
-                                emptyList()
+            fun getChoices(): List<QuestionHelper.Data.Data> {
+                return if (isTrue == null && userAnswer == null) {
+                    if (choices == null) {
+                        QuestionHelper.Data.Data.Type.values().map { it.toData() }
+                    } else {
+                        if (choices.all { it.isTrue == null && it.isSelected == null }) {
+                            choices.map {
+                                QuestionHelper.Data.Data(
+                                    id = it.id,
+                                    content = it.content,
+                                    attachmentUrl = it.attachmentUrl
+                                )
                             }
-                        }
-                    } else if (isTrue != null && userAnswer != null) {
-                        if (choices == null) {
-                            listOf(
-                                QuestionHelper.Data.Data.Type.CORRECT.toData().copy(isTrue = isTrue, isSelected = userAnswer),
-                                QuestionHelper.Data.Data.Type.INCORRECT.toData().copy(isTrue = !isTrue, isSelected = !userAnswer)
-                            )
+                        } else if (choices.all { it.isTrue != null && it.isSelected != null }) {
+                            choices.map {
+                                QuestionHelper.Data.Data(
+                                    id = it.id,
+                                    content = it.content,
+                                    isTrue = it.isTrue,
+                                    isSelected = it.isSelected,
+                                    attachmentUrl = it.attachmentUrl
+                                )
+                            }
                         } else {
                             emptyList()
                         }
+                    }
+                } else if (isTrue != null && userAnswer != null) {
+                    if (choices == null) {
+                        listOf(
+                            QuestionHelper.Data.Data.Type.CORRECT.toData().copy(isTrue = isTrue, isSelected = userAnswer),
+                            QuestionHelper.Data.Data.Type.INCORRECT.toData().copy(isTrue = !isTrue, isSelected = !userAnswer)
+                        )
                     } else {
                         emptyList()
                     }
+                } else {
+                    emptyList()
                 }
             }
-//            sealed interface Data {
-//                data class TrueFalse(
-//                    @Field("id")
-//                    val id: Int = 0,
-//                    @Field("content")
-//                    val content: String = "",
-//                    @Field("attachment_url")
-//                    val attachmentUrl: String? = null,
-//                    @Field("user_answer")
-//                    val userAnswer: Boolean? = null,
-//                    @Field("is_true")
-//                    val isTrue: Boolean? = null
-//                ) : Data
-//
-//                data class MultiChoice(
-//                    @Field("id")
-//                    val id: Int = 0,
-//                    @Field("content")
-//                    val content: String = "",
-//                    @Field("attachment_url")
-//                    val attachmentUrl: String? = null,
-//                    @Field("choices")
-//                    val choices: List<Data> = emptyList()
-//                ) : Data {
-//                    data class Data(
-//                        @Field("id")
-//                        val id: Int = 0,
-//                        @Field("content")
-//                        val content: String = "",
-//                        @Field("attachment_url")
-//                        val attachmentUrl: String? = null,
-//                        @Field("is_selected")
-//                        val isSelected: Boolean? = null,
-//                        @Field("is_true")
-//                        val isTrue: Boolean? = null
-//                    )
-//                }
-//            }
         }
 
 //        data class Data(
